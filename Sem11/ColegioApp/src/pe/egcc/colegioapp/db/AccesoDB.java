eaa798e9b31fab2b7a7003cf5f2d10559db5128d
Docente UCH <docente@soporteuch.pe>
2017-10-27 10:01:41 -0500

    Sem 11

---
+++ Sem11/ColegioApp/src/pe/egcc/colegioapp/db/AccesoDB.java
@@ -0,0 +1,42 @@
+package pe.egcc.colegioapp.db;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Gustavo Coronel
+ * @blog gcoronelc.blogspot.pe
+ * @email gcoronelc@gmail.com
+ */
+public final class AccesoDB {
+  
+   private AccesoDB() {
+  }
+
+  public static Connection getConnection() throws SQLException {
+    Connection cn = null;
+    try {
+      // Datos MySQL
+      String driver = "com.mysql.jdbc.Driver";
+      String url = "jdbc:mysql://localhost:3306/COLEGIODB";
+      String user = "colegio";
+      String pass = "admin";
+      // Cargar el driver a memoria
+      Class.forName(driver).newInstance();
+      // Obtener el objeto Connection
+      cn = DriverManager.getConnection(url, user, pass);
+    } catch (SQLException e) {
+      throw e;
+    } catch(ClassNotFoundException e){
+      throw new SQLException("ERROR, no se encuentra el driver.");
+    } catch(Exception e){
+      throw new SQLException("ERROR, no se tiene acceso al servidor.");
+    }
+    return cn;
+}
+    
+    
+}
