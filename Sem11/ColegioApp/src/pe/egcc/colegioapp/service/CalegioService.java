c3a1f84e6a5949978ceea68ddede9152a91e7861
Gustavo Coronel <gcoronelc@gmail.com>
2017-10-27 12:17:06 -0500

    Sem 11

---
+++ Sem11/ColegioApp/src/pe/egcc/colegioapp/service/CalegioService.java
@@ -0,0 +1,12 @@
+package pe.egcc.colegioapp.service;
+
+import pe.egcc.colegioapp.model.Estado;
+
+public class CalegioService {
+    
+    public  Estado crearSeccion(String nombre, int periodo, int grado, int vacantes){
+        
+        
+        return null;
+    }
+}
