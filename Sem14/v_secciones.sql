6cad4980e36bc496ca0a8de3255412b1ce1bb9c9
Docente UCH <docente@soporteuch.pe>
2017-11-17 10:06:27 -0500

    Sem14

---
+++ Sem14/v_secciones.sql
@@ -0,0 +1,13 @@
+create view v_secciones(
+  periodo, nivel, nomnivel, grado, nomgrado,
+  seccion, nombre, vacantes, matriculados
+) as
+select 
+  s.periodo, n.nivel, n.nombre,
+  g.grado, g.nombre, s.seccion,
+  s.nombre, s.vacantes, s.matriculados
+from nivel n
+join grado g on n.nivel = g.nivel
+join seccion s on g.grado = s.grado;
+
+
